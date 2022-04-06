18106404d877c114c0e1d33ccc6e73ae5aad3452
thatFemicode <leyedavid10@gmail.com>
2022-04-06 11:10:52 +0100

    preloader animation

--- src/Components/Preloader/animation.js
+++ src/Components/Preloader/animation.js
@@ -8,7 +8,17 @@ export const preloaderAnimation = (item1, item2, item3, item4, setLoading) => {
   });
   tl.to(item2, { duration: 1.4, width: '100%', ease: 'Expo.easeInOut' })
     .to(item2, { duration: 1.4, height: '100%', ease: 'Expo.easeInOut' })
-    .from(item4, { duration: 2, opacity: 0 })
+    .fromTo(
+      item4,
+      {
+        duration: 1,
+        // scale: 1.6,
+        opacity: 0,
+        scale: 0.1,
+        ease: 'Expo.easeInOut',
+      },
+      { opacity: 1, scale: 1.6 }
+    )
     .to(item3, {
       duration: 0.8,
       width: '100%',
